f215d7d6eea3d68aa1a810a219b5b44ef3cf9202
Artur Agh <aghartur@gmail.com>
2024-06-14 14:34:17 +0200

    Register Form added with validations

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/inscription', name: 'app_register')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegisterUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+
+        return $this->render('register/index.html.twig', [
+            'registerForm' => $form->createView(),
+        ]);
+    }
+}
