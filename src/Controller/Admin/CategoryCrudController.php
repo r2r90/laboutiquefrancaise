9890cc7d6101b6369ece2027c4f2fbc4e65b5ce1
Artur Agh <aghartur@gmail.com>
2024-06-16 16:45:06 +0200

    création de l'entité Product()

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -2,35 +2,34 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\User;
+use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
-class UserCrudController extends AbstractCrudController
+class CategoryCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return User::class;
+        return Category::class;
     }
 
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
-            ->setEntityLabelInSingular('Utilisateur')
-            ->setEntityLabelInPlural('Utilisateurs');
+            ->setEntityLabelInSingular('Catégorie')
+            ->setEntityLabelInPlural('Catégories');
     }
 
 
     public function configureFields(string $pageName): iterable
     {
-        // -
         return [
-            TextField::new('firstname')->setLabel('Nom'),
-            TextField::new('lastname')->setLabel('Prénom'),
-            TextField::new('email')->setLabel('Email')->onlyOnIndex(),
+            TextField::new('name')->setLabel('Titre')->setHelp("Titre de la category"),
+            SlugField::new('slug')->setLabel('URL')->setTargetFieldName('name')->setHelp('URL te votre category générée automatiquement'),
         ];
     }
 
