ad5f74a37d7dcbd29524a497d595e3539f39784c
Artur Agh <aghartur@gmail.com>
2024-06-17 15:13:53 +0200

    Product details page done

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/produit/{slug}', name: 'app_product')]
+    public function index($slug,ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->findOneBy(['slug' => $slug]);
+        return $this->render('product/index.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
